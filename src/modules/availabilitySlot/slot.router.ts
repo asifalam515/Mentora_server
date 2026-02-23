cf382622402ccb93f5e6db011699c9ccd1795e2c
asifalam515 <asibulalam515@gmail.com>
2026-02-24 05:51:37 +0600

    open availability slot for every one

--- src/modules/availabilitySlot/slot.router.ts
+++ src/modules/availabilitySlot/slot.router.ts
@@ -3,19 +3,20 @@ import { auth, UserRole } from "../../middleware/auth";
 import { slotController } from "./slot.controller";
 
 export const slotRouter = Router();
-slotRouter.post("/", auth(UserRole.TUTOR), slotController.createTimeSlot);
-// get all availability slots for a tutor, this will be used by students to see which slots are available for booking
-slotRouter.get(
-  "/tutor/:tutorId",
-  auth(UserRole.TUTOR, UserRole.STUDENT, UserRole.ADMIN),
-  slotController.getAvailabilitySlots,
-);
-// get availability slots for a tutor
 slotRouter.get(
   "/",
   auth(UserRole.TUTOR, UserRole.STUDENT, UserRole.ADMIN),
   slotController.getAvailabilitySlotsByTutorId,
 );
+slotRouter.get(
+  "/tutor/:tutorId",
+
+  slotController.getAvailabilitySlots,
+);
+// get availability slots for a tutor
+
+slotRouter.post("/", auth(UserRole.TUTOR), slotController.createTimeSlot);
+// get all availability slots for a tutor, this will be used by students to see which slots are available for booking
 
 slotRouter.delete(
   "/:slotId",
